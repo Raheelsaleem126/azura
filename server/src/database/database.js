6b71485c546f7d2baf22debc362cd64f1f1873ea
Raheel Saleem <raheelsaleem1000@gmail.com>
2023-05-31 13:49:06 +0500

    WIP

---
+++ server/src/database/database.js
@@ -0,0 +1,2 @@
+const mongoose = require('mongoose');
+module.exports = mongoose.connect('mongodb+srv://root:admin@cluster0.iqjcz.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
